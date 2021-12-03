bc224db7aea8fcc3905e40158b1a0ca706665499
will14lee <will14lee@gmail.com>
2021-12-03 05:22:57 -0500

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"] 
+    validate :is_clickbait?
+    Clickbait_Patterns =
+    [/Won't Believe/,
+    /Secret/,
+    /Top [0-9]/,
+    /Guess/]
+
+  def is_clickbait?
+    if Clickbait_Patterns.none? {|p| p.match title }
+      errors.add(:title, "Needs to be more clickbaity!")
+    end
+  end
 end
